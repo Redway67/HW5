31fbc237c8407a7d6ab5a1ba1662fc4fa9ba311e
Igor <46100831+Redway67@users.noreply.github.com>
2019-11-05 22:26:18 +0600

    Delete test_python.py

--- test_python.py
+++
@@ -1,9 +0,0 @@
-'''
-Модуль тестирования
- В модуле написать тесты для встроенных функций filter, map, sorted,
-а также для функций из библиотеки math: pi, sqrt, pow, hypot.
-'''
-
-
-def test_functions_sqrt():
-    assert sqrt(4) == 1.0
