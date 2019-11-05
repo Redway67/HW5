61cd48e8cceeb1c5b299ef05ee6ac52e60351d55
Igor <redway@mail.ru>
2019-11-05 21:29:45 +0600

    test

---
+++ test_python.py
@@ -0,0 +1,9 @@
+'''
+Модуль тестирования
+ В модуле написать тесты для встроенных функций filter, map, sorted,
+а также для функций из библиотеки math: pi, sqrt, pow, hypot.
+'''
+
+
+def test_functions_sqrt():
+    assert sqrt(4) == 1.0
